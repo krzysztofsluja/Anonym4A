0db58dba17c348dfe9b77f9d14284e6e6792e965
krzysztofsluja <slujakrzysztof6@gmail.com>
2025-03-27 17:51:14 +0100

    Implement user settings initialization by loading properties from values.properties file and update getSetting method to retrieve anonymization settings.

--- anonym4ai/src/main/java/com/sluja/anonym4ai/context/UserSettingsConfiguration.java
+++ anonym4ai/src/main/java/com/sluja/anonym4ai/context/UserSettingsConfiguration.java
@@ -1,8 +1,11 @@
 package com.sluja.anonym4ai.context;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
+import java.util.Properties;
 
 import lombok.Getter;
 
@@ -11,9 +14,28 @@ public class UserSettingsConfiguration {
     private static UserSettingsConfiguration instance;
     @Getter
     private Map<String, String> settings;
+    @Getter
+    private Map<String, String> anonymizationSettings;
 
     private UserSettingsConfiguration() {
         settings = new HashMap<>();
+        anonymizationSettings = initializeMap();
+    }
+
+    private Map<String, String> initializeMap() {
+        final Properties properties = new Properties();
+        final Map<String, String> result = new HashMap<>();
+        try (InputStream is = getClass().getClassLoader().getResourceAsStream("values.properties")) {
+            if (Objects.nonNull(is)) {
+                properties.load(is);
+                properties.forEach((key, value) -> result.put(key.toString(), value.toString()));
+            } else {
+                System.err.println("Resource not found: values.properties");
+            }
+        } catch (final IOException e) {
+            System.err.println("Failed to load properties: " + e.getMessage());
+        }
+        return result;    
     }
 
     public static UserSettingsConfiguration getInstance() {
@@ -24,7 +46,7 @@ public class UserSettingsConfiguration {
     }
 
     public String getSetting(final String key) {
-        return getInstance().getSettings().getOrDefault(key, "var");
+        return getInstance().getAnonymizationSettings().getOrDefault(key, "var");
     }
 
 }
