544c4220ea8b874dba5178fde315480149721334
slujakrzysztof <krzysiek.sluja@vp.pl>
2025-03-29 21:38:57 +0100

    Refactor AbstractCodeAnonymizer to accept AnonymizingVisitor in constructor and remove internal AnonymizingVisitor class.

--- anonym4ai/src/main/java/com/sluja/anonym4ai/anonymization/AbstractCodeAnonymizer.java
+++ anonym4ai/src/main/java/com/sluja/anonym4ai/anonymization/AbstractCodeAnonymizer.java
@@ -8,17 +8,26 @@ import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.Node;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 import com.github.javaparser.ast.body.EnumDeclaration;
+import com.github.javaparser.ast.body.FieldDeclaration;
 import com.github.javaparser.ast.body.MethodDeclaration;
+import com.github.javaparser.ast.body.Parameter;
 import com.github.javaparser.ast.body.VariableDeclarator;
+import com.github.javaparser.ast.stmt.BlockStmt;
 import com.github.javaparser.ast.visitor.VoidVisitorAdapter;
+import com.sluja.anonym4ai.anonymization.utils.AnonymizingVisitor;
 import com.sluja.anonym4ai.context.UserSettingsConfiguration;
+
+import lombok.Getter;
 
 public abstract class AbstractCodeAnonymizer<T, K extends Node> {
 
     final protected UserSettingsConfiguration userSettingsConfiguration;
+    @Getter
+    final protected AnonymizingVisitor visitor;
 
-    public AbstractCodeAnonymizer() {
+    public AbstractCodeAnonymizer(final AnonymizingVisitor visitor) {
         userSettingsConfiguration = UserSettingsConfiguration.getInstance();
+        this.visitor = visitor;
     }
 
     public abstract String anonymize(final T code);
@@ -29,50 +38,4 @@ public abstract class AbstractCodeAnonymizer<T, K extends Node> {
         return userSettingsConfiguration.getSetting(key);
     }
 
-    public class AnonymizingVisitor extends VoidVisitorAdapter<Void> {
-
-        private final Map<Class<?>, BiConsumer<Object, Void>> handlers = new HashMap<>();
-
-        public <N> void registerHandler(Class<N> nodeClass, BiConsumer<N, Void> handler) {
-            handlers.put(nodeClass, (BiConsumer<Object, Void>) handler);
-        }
-
-        private void executeAnonymization(final Class<?> nodeClass, final Object node, final Void arg) {
-            handlers.computeIfPresent(nodeClass, (k, v) -> {
-                v.accept(node, arg);
-                return v;
-            });
-        }
-
-        @Override
-        public void visit(final ClassOrInterfaceDeclaration n, Void arg) {
-            executeAnonymization(ClassOrInterfaceDeclaration.class, n, arg);
-            super.visit(n, arg);
-        }
-
-        @Override
-        public void visit(final MethodDeclaration n, Void arg) {
-            executeAnonymization(MethodDeclaration.class, n, arg);
-            super.visit(n, arg);
-        }
-
-        @Override
-        public void visit(final VariableDeclarator n, Void arg) {
-            executeAnonymization(VariableDeclarator.class, n, arg);
-            super.visit(n, arg);
-        }
-
-        @Override
-        public void visit(final CompilationUnit n, Void arg) {
-            executeAnonymization(CompilationUnit.class, n, arg);
-            super.visit(n, arg);
-        }
-
-        @Override
-        public void visit(final EnumDeclaration n, Void arg) {
-            executeAnonymization(EnumDeclaration.class, n, arg);
-            super.visit(n, arg);
-        }
-
-    }
 }
--- anonym4ai/src/main/java/com/sluja/anonym4ai/anonymization/AbstractUnifiedCodeAnonymizer.java
+++ anonym4ai/src/main/java/com/sluja/anonym4ai/anonymization/AbstractUnifiedCodeAnonymizer.java
@@ -1,7 +1,12 @@
 package com.sluja.anonym4ai.anonymization;
 
 import com.github.javaparser.ast.Node;
+import com.sluja.anonym4ai.anonymization.utils.AnonymizingVisitor;
 
-public abstract class AbstractUnifiedCodeAnonymizer<T extends Node> extends AbstractCodeAnonymizer<T, T>{
+public abstract class AbstractUnifiedCodeAnonymizer<T extends Node> extends AbstractCodeAnonymizer<T, T> {
+
+    public AbstractUnifiedCodeAnonymizer(final AnonymizingVisitor visitor) {
+        super(visitor);
+    }
 
 }
