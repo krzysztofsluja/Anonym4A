1b4b8ff5dcbee3f092d5de335b16a0034a1ca0c1
krzysztofsluja <slujakrzysztof6@gmail.com>
2025-03-30 12:54:37 +0200

    Add StatementDispatcher class to manage statement visitors for improved statement anonymization handling.

---
+++ anonym4ai/src/main/java/com/sluja/anonym4ai/anonymization/utils/statement/dispatcher/StatementDispatcher.java
@@ -0,0 +1,42 @@
+package com.sluja.anonym4ai.anonymization.utils.statement.dispatcher;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Objects;
+
+import com.github.javaparser.ast.stmt.Statement;
+import com.sluja.anonym4ai.anonymization.utils.statement.visitor.AbstractStatementAnonymizingVisitor;
+import com.sluja.anonym4ai.anonymization.utils.statement.visitor.BasicStatementVisitor;
+import com.sluja.anonym4ai.anonymization.utils.statement.visitor.ControlFlowStatementVisitor;
+import com.sluja.anonym4ai.anonymization.utils.statement.visitor.ExceptionStatementVisitor;
+
+public class StatementDispatcher {
+    private final Map<Class<? extends Statement>, AbstractStatementAnonymizingVisitor> visitorMap;
+    private final AbstractStatementAnonymizingVisitor basicVisitor;
+    private final AbstractStatementAnonymizingVisitor controlFlowVisitor;
+    private final AbstractStatementAnonymizingVisitor exceptionVisitor;
+
+    public StatementDispatcher() {
+        this.visitorMap = new HashMap<>();
+        this.basicVisitor = new BasicStatementVisitor();
+        this.controlFlowVisitor = new ControlFlowStatementVisitor();
+        this.exceptionVisitor = new ExceptionStatementVisitor();
+        registerVisitors();
+    }
+
+    private void registerVisitors() {
+        basicVisitor.getHandledStatements().forEach(type -> 
+            visitorMap.put(type, basicVisitor));
+        controlFlowVisitor.getHandledStatements().forEach(type -> 
+            visitorMap.put(type, controlFlowVisitor));
+        exceptionVisitor.getHandledStatements().forEach(type -> 
+            visitorMap.put(type, exceptionVisitor));
+    }
+
+    public void dispatchStatement(final Statement statement) {
+        final AbstractStatementAnonymizingVisitor visitor = visitorMap.get(statement.getClass());
+        if (Objects.nonNull(visitor)) {
+            statement.accept(visitor, null);
+        }
+    }
+}
