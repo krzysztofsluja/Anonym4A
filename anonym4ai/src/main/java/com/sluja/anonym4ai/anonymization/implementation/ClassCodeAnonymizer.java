92260730b35b7ad75a9c28dd8c860f2b79d61129
slujakrzysztof <krzysiek.sluja@vp.pl>
2025-03-30 19:42:12 +0200

    Add method to remove annotations from class declaration

--- anonym4ai/src/main/java/com/sluja/anonym4ai/anonymization/implementation/ClassCodeAnonymizer.java
+++ anonym4ai/src/main/java/com/sluja/anonym4ai/anonymization/implementation/ClassCodeAnonymizer.java
@@ -1,6 +1,7 @@
 package com.sluja.anonym4ai.anonymization.implementation;
 
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
+import com.github.javaparser.ast.expr.AnnotationExpr;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.sluja.anonym4ai.anonymization.AbstractCodeAnonymizer;
 import com.sluja.anonym4ai.anonymization.interfaces.ICounterReset;
@@ -24,9 +25,14 @@ public class ClassCodeAnonymizer extends AbstractCodeAnonymizer<ClassOrInterface
     }
 
     private void anonymizeClass(final ClassOrInterfaceDeclaration code, final Void arg) {
+        removeAnnotations(code);
         final String originalName = code.getNameAsString();
         final String anonymizedName = userSettingsConfiguration.getSetting("class.name") + classCounter++;
         code.setName(anonymizedName);
+    }
+
+    private void removeAnnotations(final ClassOrInterfaceDeclaration code) {
+        code.getAnnotations().forEach(AnnotationExpr::remove);
     }
 
     @Override
